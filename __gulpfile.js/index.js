c03e204dd7ac18cd3949056a6ad76b8cce1e6b78
unknown <al.tulin@gmail.com>
2021-04-24 09:25:55 +0800

    new gulp

--- __gulpfile.js/index.js
+++ __gulpfile.js/index.js
@@ -3,11 +3,10 @@ const util = require(`./modules/util.js`).util;
 const tasks = require(`./modules/tasks.js`).tasksObject;
 
 
-const defaultArray = [`cleanImg`, `makeImagesSvg`, `makeImages`, `createSprite`, `getStyleFile`, `getScriptFile`, `browsersync`];
+// const defaultArray = [`cleanImg`, `makeImagesSvg`, `makeImages`, `createSprite`, `getStyleFile`, `getScriptFile`, `browsersync`];
+const defaultArray = [`getWatchers`, `browsersync`];
 
-const buildArray = [`cleanImg`]
+const buildArray = [`clean`, `copying`]
 
 exports.default = plugins.gulp.series(util.getTasks(defaultArray, tasks));
 exports.build = plugins.gulp.series(util.getTasks(buildArray, tasks))
-
-
--- __gulpfile.js/modules/copying.js
+++ __gulpfile.js/modules/copying.js
@@ -1,17 +1,21 @@
+const path = require(`${__dirname}/variables.js`).path;
+const pluginsPath = require(`${__dirname}/plugins.js`).plaginsObject;
 
+const src = pluginsPath.gulp.src;
+const dest = pluginsPath.gulp.dest;
+const path_dist = path.dist
 
 module.exports.getCopying = () => {
   return src([ // Выбираем нужные файлы
-    path.src.css,
-    path.src.fonts,
-    path.src.min_js,
-    path.src.js,
-    `${sourceFolder}/js/modules/*.js`,
-    path.src.img,
-    path.src.html,
-    path.src.ico,
-    path.src.fav,
-    path.src.files
-  ], { base: `${sourceFolder}` }) // Параметр "base" сохраняет структуру проекта при копировании
-    .pipe(dest(`${projectFolder}/`)) // Выгружаем в папку с финальной сборкой
+    path_dist.cssFile,
+    path_dist.cssMinFile, // стили мин если надо
+    path_dist.fontsFiles,
+    path_dist.jsFile,
+    path_dist.jsMinFile, // скрипты мин если надо
+    path_dist.images,
+    path_dist.htmlFiles,
+    path_dist.icoFiles,
+    path_dist.favFiles,
+  ], { base: path.srcFolder }) // Параметр "base" сохраняет структуру проекта при копировании
+    .pipe(dest(`${path.distFolder}/`)) // Выгружаем в папку с финальной сборкой
 };
